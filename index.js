630e007480728a405d90c0c5ac3cd6e63b6e3ce7
PascualVera <pascualvera.994@gmail.com>
2022-08-25 19:39:22 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const { chromium } = require("playwright");
+
+const shops = [
+	{
+		vendor: "Carrefour",
+		url: "https://www.carrefour.es/playstation-5-digital-825gb-blanco-2-dualsense-pulse-3d-negro-psn-50/VC4A-18742548/p",
+		checkStock: async ({ page }) => {
+			const content = await page.textContent(".add-to-cart-button");
+			return content.includes("Agotado temporalmente") === false;
+		},
+	},
+	{
+		vendor: "Amazon",
+		url: "https://www.amazon.es/Playtation-Disc-Versi%C3%B3n-PS-5-Consola/dp/B09YQLQ2LJ/ref=sr_1_1?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=ps5+consola&qid=1661448092&refinements=p_89%3ASony&rnid=1692911031&s=videogames&sr=1-1",
+		checkStock: async ({ page }) => {
+			const content = await page.textContent(".a-color-price");
+			return content.includes("No disponible.") === false;
+		},
+	},
+];
+(async () => {
+	const browser = await chromium.launch();
+
+	for (const shop of shops) {
+		const { checkStock, vendor, url } = shop;
+		const page = await browser.newPage(url);
+		page.goto(url);
+		const hasStock = await checkStock({ page });
+		console.log(`${vendor}: ${hasStock ? "hasStock" : "out of stock"}`);
+	}
+
+	await browser.close();
+})();
